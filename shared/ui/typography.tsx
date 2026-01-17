d66255ad9ac5c8ea451b5a4007f659dfa018fb47
Sarun Daunghirun <sdsarun@outlook.com>
2026-01-17 16:28:05 +0700

    fix(typography): update variants in headingVariants
    Revised the variant options in headingVariants by removing dark mode classes and unused variants, and adding new variants h0, body0, and p0 with simplified styles.

--- shared/ui/typography.tsx
+++ shared/ui/typography.tsx
@@ -5,15 +5,11 @@ import { cva, type VariantProps } from "class-variance-authority";
 const headingVariants = cva("", {
   variants: {
     variant: {
-      h1: "font-semibold text-base dark:text-white",
-      h2: "font-semibold text-sm dark:text-white",
-      body1: "font-medium text-xs",
-      body2: "font-medium text-xs dark:text-white",
-      body3: "font-medium text-sm dark:text-white",
-      p1: "text-sm text-muted-foreground",
-      p2: "text-xs text-muted-foreground",
-      p3: "text-sm text-muted-foreground dark:text-white",
-      p4: "text-xs text-muted-foreground dark:text-white"
+      h0: "",
+      h1: "font-semibold text-base",
+      h2: "font-semibold text-sm",
+      body0: "",
+      p0: "text-sm"
     }
   },
   defaultVariants: {
