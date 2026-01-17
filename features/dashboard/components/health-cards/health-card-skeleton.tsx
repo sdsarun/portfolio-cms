acb92d9d9f3a370b15a81b429460614ae67b7661
Sarun Daunghirun <sdsarun@outlook.com>
2026-01-18 01:12:52 +0700

    refactor(dashboard): health card components layout and skeleton
    Updated HealthCardItem and HealthCardSkeleton to improve layout consistency. Replaced CardContent with CardHeader and CardAction in skeleton, adjusted class names, and improved structure for better alignment and maintainability.

--- features/dashboard/components/health-cards/health-card-skeleton.tsx
+++ features/dashboard/components/health-cards/health-card-skeleton.tsx
@@ -1,23 +1,25 @@
 // components
 import { Skeleton } from "@/shared/ui/skeleton";
-import { Card, CardContent } from "@/shared/ui/card";
+import { Card, CardAction, CardHeader } from "@/shared/ui/card";
 import { Box } from "@/shared/layout/box";
 
 export function HealthCardSkeleton() {
   return (
     <Box as="section" className="grid gap-4 md:grid-cols-2">
       {[1, 2].map((i) => (
-        <Card key={i}>
-          <CardContent className="p-4 flex items-center justify-between">
-            <Box className="flex items-center gap-3">
+        <Card key={i} className="py-4">
+          <CardHeader>
+            <Box className="row-span-2 flex gap-2">
               <Skeleton className="h-9 w-9 rounded-full" />
-              <Box className="space-y-2">
+              <Box className="flex flex-col gap-2">
                 <Skeleton className="h-4 w-24" />
                 <Skeleton className="h-3 w-16" />
               </Box>
             </Box>
-            <Skeleton className="h-8 w-20" />
-          </CardContent>
+            <CardAction className="self-center-safe">
+              <Skeleton className="h-8 w-24" />
+            </CardAction>
+          </CardHeader>
         </Card>
       ))}
     </Box>
